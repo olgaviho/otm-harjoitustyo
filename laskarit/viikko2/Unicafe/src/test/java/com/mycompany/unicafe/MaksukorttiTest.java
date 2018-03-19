2311af5496f2e7fe71867c3a9a709b0b35f5d770
Olga Viholainen <olga.viholainen@helsinki.fi>
2018-03-19 14:34:46 +0200

    added tests to unicafe

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -17,4 +17,44 @@ public class MaksukorttiTest {
     public void luotuKorttiOlemassa() {
         assertTrue(kortti!=null);      
     }
+    
+    @Test
+    public void lataaRahaaOnnistuu() {
+        kortti.lataaRahaa(10);
+        assertEquals("saldo: 0.20", kortti.toString());
+        
+    }
+    @Test
+    public void lataaRahaaOnnistuuNegatiivisesti() {
+        kortti.lataaRahaa(-5);
+        assertEquals("saldo: 0.5", kortti.toString());
+        
+    }
+    
+    @Test
+    public void toStringToimii() {
+        assertEquals("saldo: 0.10", kortti.toString());
+        
+    }
+    
+    @Test
+    public void saldoToimii() {
+        assertEquals(10, kortti.saldo());
+        
+    }
+    
+    @Test
+    public void OtaRahaaToimii() {
+        kortti.otaRahaa(5);
+        assertEquals("saldo: 0.5", kortti.toString());
+        
+    }
+    
+    @Test
+    public void OtarahaaEiToimiYliSaldon() {
+        kortti.otaRahaa(15);
+        assertEquals("saldo: 0.10", kortti.toString());
+        
+    }
+    
 }
